f8f2e08aa38a29f0155d73e4899711e489244eb0
Alamsyah-WM <alamsyahmassie18@gmail.com>
2024-12-05 21:23:45 +0700

    Update and add all API w JS

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const loadModel = require('./models/loadModel');
-const recommendationRoutes = require('./routes/recommendationRoutes');
 const placeRoutes = require('./routes/placeRoutes');
 const mostVisitedRoutes = require('./routes/mostVisitedRoutes');
 const nearestRoutes = require('./routes/nearestRoutes');
@@ -18,28 +16,10 @@ const nearestRoutes = require('./routes/nearestRoutes');
         },
     });
 
-    
-    const model = await loadModel();
-    server.app.model = model;
- 
     server.route(placeRoutes);  
     server.route(mostVisitedRoutes);  
     server.route(nearestRoutes);  
-    server.route(recommendationRoutes);  
-    server.ext('onPreResponse', function (request, h) {
-      const response = request.response;
-      if (response.isBoom) {  
-          const newResponse = h.response({
-              status: 'fail',
-              message: response.message
-          });
-          newResponse.code(response.output.statusCode);  
-          return newResponse;
-      }
-      return h.continue;
-  });
-  
- 
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
